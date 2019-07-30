376c117fb34daa73d77a266f25b221057b1ad875
AbijithSubramaniyan <49241439+AbijithSubramaniyan@users.noreply.github.com>
2019-07-30 23:57:17 +0530

    Initial commit

---
+++ second.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+
+function getdetails(){
+  var countryData = JSON.parse(localStorage.getItem('x'));
+  document.getElementById("capital").innerHTML = countryData.capital;
+  document.getElementById("calling-code").innerHTML = `+${countryData.callingCodes[0]}`;
+  document.getElementById("population").innerHTML = countryData.population.toLocaleString();
+  document.getElementById("currencies").innerHTML = countryData.currencies.filter(c => c.name).map(c => `${c.name} (${c.code})`).join(", ");
+  document.getElementById("region").innerHTML = countryData.region;
+  document.getElementById("subregion").innerHTML = countryData.subregion;
+}
+function goBack(){
+
+  window.history.back();
+}
